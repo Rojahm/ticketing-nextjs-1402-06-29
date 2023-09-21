5762cd865876d245e5a646be7879b8d114657e47
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-21 15:43:01 +0330

    init new ticketing project with supabase template for netx js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
+        primary: "#DC50EE",
+        secondary: "#F5C8F9",
+        tertiary: "#FBECFD",
+        warning: "#A6FF00",
+        text: "#292929",
         btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
+          background: "hsl(var(--btn-background))",
+          "background-hover": "hsl(var(--btn-background-hover))",
         },
       },
     },
   },
   plugins: [],
-}
+};
