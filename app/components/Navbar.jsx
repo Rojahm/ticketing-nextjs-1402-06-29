fb8f5d7f076fbd24c2d0076682904324e7d0d0b9
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-21 21:49:26 +0330

    react icon

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
+import { FaSignInAlt } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <Image
-        src={
-          "/img/lssi-high-resolution-logo-color-on-transparent-background.webp"
-        }
-        width={120}
-        height={120}
-        alt="site logo"
-      />
+      <Link href={"/"}>
+        <Image
+          src={
+            "/img/lssi-high-resolution-logo-color-on-transparent-background.webp"
+          }
+          width={120}
+          height={120}
+          alt="site logo"
+        />
+      </Link>
       <nav>
         <Link href={"/"}>تیکت</Link>
         <Link href={"/"}>تقویم</Link>
@@ -19,6 +22,12 @@ function Navbar() {
         <Link href={"/"}>قرارداد</Link>
         <Link href={"/"}>بلاگ</Link>
       </nav>
+      <div className="user-nav">
+        <Link href={"/login"} className="flex items-center">
+          <FaSignInAlt size={20} />
+          <span className="mr-2">ورود</span>
+        </Link>
+      </div>
     </div>
   );
 }
